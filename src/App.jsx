cfa69416f934e35482895226e4bc60aca7a8eb2d
Yashs Tiwari <yashstiwari03@gmail.com>
2023-10-08 20:26:20 +0530

    added login page route and made it protected by making it wrapped into private route and added auth context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import Room from "./Pages/room";
+import LoginPage from "./Pages/LoginPage";
+import PrivateRoutes from "./components/PrivateRoutes";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./utils/AuthContext";
 
 function App() {
   return (
-    <>
-      <Room />
-    </>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route element={<PrivateRoutes />}>
+            <Route path="/" element={<Room />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
